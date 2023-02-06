f92dcdcee84ec6bff9650779f2fd314a8cfacef7
AnnaViktorovna <anna.bazileeva@gmail.com>
2023-02-05 20:05:01 -0500

    calc

---
+++ Loop.js
@@ -0,0 +1,18 @@
+const car = {
+    make:"Nissan",
+    year: 2022,
+    used: true
+}
+
+delete car.make
+car.year = 2023
+car.color = 'silver'
+
+console.log(car)
+console.log(car.used)
+
+
+for([начало]; [условие]; [шаг]) выражения {
+
+}
+
